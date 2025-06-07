3c2154230a366fddc901051bd35bf41ed78e1c44
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-06-07 17:10:38 -0400

    Initial Commit

---
+++ eSwiftCommerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eSwiftCommerce
+//
+//  Created by Bonmyeong Koo - Vendor on 6/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eSwiftCommerce/eSwiftCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  eSwiftCommerceApp.swift
+//  eSwiftCommerce
+//
+//  Created by Bonmyeong Koo - Vendor on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct eSwiftCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
